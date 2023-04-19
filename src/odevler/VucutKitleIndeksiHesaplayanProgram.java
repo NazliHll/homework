29c8b2a57aaf56200bf86739f95091c5042de470
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-19 11:53:00 +0300

    19.04.2023

--- src/odevler/VucutKitleIndeksiHesaplayanProgram.java
+++ src/odevler/VucutKitleIndeksiHesaplayanProgram.java
@@ -15,12 +15,13 @@ Lütfen kilonuzu giriniz : 105
 Vücut Kitle İndeksiniz : 35.49215792320173*/
 
     public static void main(String[] args) {
-        Scanner scan= new Scanner(System.in);
+      Scanner scan= new Scanner(System.in);
         System.out.print("Lütfen Kilonuzu Kilogram Cinsinden Giriniz: ");
         double kilo= scan.nextDouble();
         System.out.print("Lütfen Boyunuzu Metre Cinsinden Giriniz: ");
         double boy= scan.nextDouble();
         double vKitleInd= kilo/(boy*boy);
         System.out.println("Vücut Kitle İndeksiniz: "+vKitleInd);
+
     }
 }
