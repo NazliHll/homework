7834dfa47f7e633a5c2930e8f1fc0db92e09f7c8
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-22 23:44:19 +0300

    22.04.2023

--- src/odevler/SayilariBuyuktenKucugeSiralama.java
+++ src/odevler/SayilariBuyuktenKucugeSiralama.java
@@ -17,5 +17,25 @@ public class SayilariBuyuktenKucugeSiralama {
         System.out.print("Üçüncü Sayiyi Giriniz : ");
         c = input.nextInt();
 
+        if ((a>b)&&(a>c)){
+            if (b>c){
+                System.out.println("Sıralama : "+ a +" > "+ b + " > " + c);
+            }else {
+                System.out.println("Sıralama : "+ a + " > " + " > " + c + " > " + b);
+            }
+        } else if ((b>c) &&(b>a)) {
+            if (a>c){
+                System.out.println("Sıralama : "+ b + " > " + a + " > " + c);
+            }else{
+                System.out.println("Sıralama : "+ b +" > " + c + " > " + a);
+            }
+        }else {
+            if ((b>a)){
+                System.out.println("Sıralama : "+ c + " > " + b + " > " + a);
+            }else {
+                System.out.println("Sıralama : "+ c + " > " + a + " > " + b);
+            }
+        }
+
     }
 }
