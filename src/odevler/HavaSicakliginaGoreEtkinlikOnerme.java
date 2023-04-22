b4ae0cb4155ab1c4dc8f0651408829dd03543d13
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-22 23:38:44 +0300

    22.04.2023

--- src/odevler/HavaSicakliginaGoreEtkinlikOnerme.java
+++ src/odevler/HavaSicakliginaGoreEtkinlikOnerme.java
@@ -1,7 +1,15 @@
 package odevler;
 
+import java.util.Scanner;
+
 public class HavaSicakliginaGoreEtkinlikOnerme {
     public static void main(String[] args) {
+        int heat;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hava sicakligini giriniz : ");
+
+        heat = input.nextInt();
 
     }
 }
