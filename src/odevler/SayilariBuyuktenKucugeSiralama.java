819052499a2973bae8dda8b4c5edc145718fd357
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-22 23:43:43 +0300

    22.04.2023

--- src/odevler/SayilariBuyuktenKucugeSiralama.java
+++ src/odevler/SayilariBuyuktenKucugeSiralama.java
@@ -1,9 +1,21 @@
 package odevler;
 
+import java.util.Scanner;
+
 public class SayilariBuyuktenKucugeSiralama {
     public static void main(String[] args) {
         //Ödev
         //Girilen 3 sayıyı "küçükten büyüğe" sıralayan programı yazınız.
+        Scanner input = new Scanner(System.in);
+
+        int a,b,c,sonuc;
+
+        System.out.print("Birinci Sayiyi Giriniz : ");
+        a = input.nextInt();
+        System.out.print("İkinci Sayiyi Giriniz : ");
+        b = input.nextInt();
+        System.out.print("Üçüncü Sayiyi Giriniz : ");
+        c = input.nextInt();
 
     }
 }
