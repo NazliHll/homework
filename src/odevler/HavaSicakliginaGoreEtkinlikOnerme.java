496be124bbf17348b54612ca6197e142d1047c88
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-22 23:39:55 +0300

    22.04.2023

--- src/odevler/HavaSicakliginaGoreEtkinlikOnerme.java
+++ src/odevler/HavaSicakliginaGoreEtkinlikOnerme.java
@@ -1,6 +1,14 @@
 package odevler;
 
 import java.util.Scanner;
+/*Koşullar :
+
+Sıcaklık 5'dan küçük ise "Kayak" yapmayı öner.
+Sıcaklık 5 ve 15 arasında ise "Sinema" etkinliğini öner.
+Sıcaklık 15 ve 25 arasında ise "Piknik" etkinliğini öner.
+Sıcaklık 25'ten büyük ise "Yüzme" etkinliğini öner.
+Ödev
+Aynı örnek üzerinden if koşulları başka hangi şekilde oluşturulabilirdi farklı çözüm yolları bulunuz.*/
 
 public class HavaSicakliginaGoreEtkinlikOnerme {
     public static void main(String[] args) {
@@ -10,6 +18,14 @@ public class HavaSicakliginaGoreEtkinlikOnerme {
         System.out.println("Hava sicakligini giriniz : ");
 
         heat = input.nextInt();
-
+        if (heat >= 25){
+            System.out.println("Yüzmeye gidebilirsin");
+        }else if(heat >= 10){
+            System.out.println("Piknige gidebilirsin");
+        }else if(heat>=5){
+            System.out.println("Sinemaya gidebilirsin");
+        }else if(heat>=0){
+            System.out.println("Kayak Yapabilirsin");
+        }
     }
 }
