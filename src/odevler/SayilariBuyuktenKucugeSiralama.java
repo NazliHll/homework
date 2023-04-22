ce29f11cc300697db4bcb00ba398591934b8321d
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-04-22 23:42:54 +0300

    22.04.2023

---
+++ src/odevler/SayilariBuyuktenKucugeSiralama.java
@@ -0,0 +1,9 @@
+package odevler;
+
+public class SayilariBuyuktenKucugeSiralama {
+    public static void main(String[] args) {
+        //Ödev
+        //Girilen 3 sayıyı "küçükten büyüğe" sıralayan programı yazınız.
+
+    }
+}
