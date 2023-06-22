e796a4aa06011d87a71e1cb606ae1c2f7ccbafb4
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-06-22 13:31:58 +0300

    22.06.2023

---
+++ src/odevler/TekSayilarinToplaminiBulanProgram.java
@@ -0,0 +1,14 @@
+package odevler;
+
+public class TekSayilarinToplaminiBulanProgram {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sayi;
+        int toplamCift = 0;
+        int toplamDordunKati = 0;
+
+        do {
+            System.out.print("Lutfen bir tam sayi giriniz: ");
+            sayi = input.nextInt();
+    }
+}
