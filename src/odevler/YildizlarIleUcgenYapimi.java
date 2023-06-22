6fea755ba11a0898f542b2cf85a7964635e56c6b
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-06-22 18:03:20 +0300

    22.06.2023

---
+++ src/odevler/YildizlarIleUcgenYapimi.java
@@ -0,0 +1,35 @@
+package odevler;
+
+import java.util.Scanner;
+
+public class YildizlarIleUcgenYapimi {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Pozitif bir tam sayi giriniz: ");
+        int sayi = scan.nextInt();
+
+        for (int i = 1; i <= sayi; i++) {
+            for (int j = sayi; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+        for (int a = 1; a <= sayi; a++) {
+            for (int c = 0; c < a; c++) {
+                System.out.print(" ");
+            }
+
+            for (int b = 0; b < (2 * sayi) - (2 * a + 1); b++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
