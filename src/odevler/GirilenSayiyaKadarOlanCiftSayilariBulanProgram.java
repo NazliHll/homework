8dbdbd31b71fd02f9b64de22078abf0c04d12ac6
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-06-22 13:28:56 +0300

    22.06.2023

--- src/odevler/GirilenSayiyaKadarOlanCiftSayilariBulanProgram.java
+++ src/odevler/GirilenSayiyaKadarOlanCiftSayilariBulanProgram.java
@@ -1,5 +1,7 @@
 package odevler;
 
+import java.util.Scanner;
+
 public class GirilenSayiyaKadarOlanCiftSayilariBulanProgram {
     public static void main(String[] args) {
         int k, piece = 0, total = 0;
